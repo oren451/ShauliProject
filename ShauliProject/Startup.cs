58bcc39a4d5faf8d5977121a22aba99f21b7c3f2
תומר <@LAPTOP-V5DPN9TH>
2017-09-10 19:51:47 +0300

    Add project files.

---
+++ ShauliProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShauliProject.Startup))]
+namespace ShauliProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
